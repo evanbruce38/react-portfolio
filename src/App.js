438f0b5ac69af2b6037b44fedc39d54a2d03a10c
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-11-08 15:12:23 -0500

    app.js components resume bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
-import Resume from './Components/Resume';
+import Resume from './components/Resume';
 
 function App() {
 
