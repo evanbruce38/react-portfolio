bbbdd57bd231a96d8e86628f44787a785b4422e5
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-11-07 22:43:59 -0500

    fixing resume link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import resume from '../../assets/Resume';
+import resume from '../../assets/Resume.pdf';
 
 function Resume() {
     return (
@@ -8,7 +8,7 @@ function Resume() {
             <section id="header-img-partial"></section>
 
             <div className="container resume-container">
-                <a href={resume} target="_blank" rel='noreferrer'><button className="btn btn-success btn-lg mt-5 mb-5">Link to Resume Here</button></a>
+                <a href={resume} target="_blank" rel='noreferrer'><button className="btn btn-success btn-lg mt-5 mb-5">Link to Resume</button></a>
                 <h1 className='d-flex justify-content-center profs mb-5'>Proficiencies</h1>
                     <div className='ul-container d-flex justify-content-between'>
                         <ul className='mt-3 mb-5 ml-5'>
